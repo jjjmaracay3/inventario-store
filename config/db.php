a3723f6f93ad9cb2b450b715f304cbcbf83c9470
Jhonny Maracay <jjjmaracay3@gmail.com>
2023-02-06 23:30:17 -0400

    agregando acceso remoro a gii

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=dbiventario',
     'username' => 'root',
-    'password' => '',
+    'password' => 'db.123',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
