977de3af0d80db53e1b6c56c80159332c7d5688a
betsy <concienciaenplenitud@gmail.com>
2023-02-09 21:44:42 +0000

    betsy

--- requirements.php
+++ requirements.php
@@ -9,6 +9,7 @@
  * If you are using Linux you can create a hard link instead, using the following command:
  * ln ../requirements.php requirements.php
  */
+#
 
 // you may need to adjust this path to the correct Yii framework path
 // uncomment and adjust the following line if Yii is not located at the default path
